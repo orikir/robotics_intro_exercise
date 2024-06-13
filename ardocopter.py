9c1361d13f231d33faf03b20438751d32bed8886
orikir <orikirshenboim@gamil.com>
2024-06-13 09:30:59 -0700

    2 drones comunication - work but simple actions don't

--- ardocopter.py
+++ ardocopter.py
@@ -1,19 +1,48 @@
+import threading
 import time
+import socket
+
 from dronekit import connect, VehicleMode, LocationGlobalRelative
 
 
+def is_leader(config_file_path):
+    """
+    Read configuration file to determine leader or follower mode
+    """
+    with open(config_file_path, "r") as file:
+        mode = file.readline().strip()
+
+    # Assuming configuration file contains a single line: "leader" or "follower"
+    if mode == "leader":
+        return True
+    elif mode == "follower":
+        return False
+    else:
+        return None
+
+
 class Copter:
-    def _connect(self, address, system_id):
+    def _connect(self, address):
         """
         Connect to the Vehicle.
         """
-        print("Connecting to vehicle on: ", address, system_id)
+        print("Connecting to vehicle on: ", address)
         self.vehicle = connect(address, wait_ready=True)
 
-    def __init__(self, address, system_id, is_leader=True):
-        self._connect(address, system_id)
-        self.system_id = system_id
+    def __init__(self, address, is_leader):
+        self._connect(address)
+
+        # self.is_leader = is_leader("config.txt")
         self.is_leader = is_leader
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.follower_address = ('127.0.0.1', 12345)  # Follower's address
+
+        if self.is_leader:
+            pass
+
+        elif not self.is_leader:
+            print("Start binding...")
+            self.socket.bind(self.follower_address)  # Bind for receiving
 
     def print_info(self):
         """
@@ -69,20 +98,54 @@ class Copter:
                 break
             time.sleep(1)
 
-    def fly_to_location(self, latitude, longitude, altitude):
+    def fly_to_location(self, latitude, longitude, altitude, delay=30):
         """
         Fly to the given location.
         """
-        print("Going towardspoint for 30 seconds ...")
+        print(f"Going to location: {latitude} {longitude} {altitude} for {delay} seconds")
         point = LocationGlobalRelative(latitude, longitude, altitude)
         self.vehicle.simple_goto(point)
 
         # sleep so we can see the change in map
-        time.sleep(30)
+        time.sleep(delay)
 
     def get_location(self):
+        """
+        Get current location
+        :return: latitude longitude and altitude
+        """
         global_frame = self.vehicle.location.global_relative_frame
         return global_frame.lat, global_frame.lon, global_frame.alt
+
+    def send_position_to_follower(self):
+        while True:
+            # Get current position of the leader drone
+            lat, lon, alt = self.get_location()
+
+            # Send position to follower
+            message = f"Position: {lat}, {lon}, {alt}"
+            self.socket.sendto(message.encode(), self.follower_address)
+            print("message was sent: " + message)
+
+            # Sleep for some time before sending the next position
+            time.sleep(1)
+
+    def navigate_to_leader_position(self):
+        # self.arm()
+        # self.takeoff(5)
+
+        while True:
+        #     if self.vehicle.mode.name != "GUIDED":
+        #         print("User has changed flight modes - aborting follow-me")
+        #         break
+
+            data, _ = self.socket.recvfrom(1024)  # Adjust buffer size as needed
+            leader_position = data.decode().split(":")[1].strip()  # Extract leader position from message
+            print("message was received" + leader_position)
+            # Convert leader position to our format
+            leader_lat, leader_lon, leader_alt = map(float, leader_position.split(","))
+
+            self.fly_to_location(leader_lat, leader_lon, leader_alt, 2)
 
     def return_home(self):
         """
--- main.py
+++ main.py
@@ -4,17 +4,24 @@ Small script using dronekit
 from ardocopter import *
 
 ADDRESS = 'udpin:0.0.0.0:14550'
+ADDRESS2 = 'udpin:0.0.0.0:14560'
 
 
 def main():
-    copter = Copter(ADDRESS, 1)
-    copter.arm()
-    copter.takeoff(10)
-    print(copter.get_location())
-    copter.fly_to_location(-35.361354, 149.165218, 20)
-    print(copter.get_location())
-    copter.return_home()
-    copter.disconnect()
+    follower_copter = Copter(ADDRESS2, False)
+
+    leader_copter = Copter(ADDRESS, True)
+    # leader_copter.arm()
+    # leader_copter.takeoff(10)
+    # leader_copter.fly_to_location(-35.361354, 149.165218, 20)
+
+    navigate_thread = threading.Thread(target=follower_copter.navigate_to_leader_position)
+    navigate_thread.daemon = True
+    navigate_thread.start()
+
+    navigate_thread = threading.Thread(target=leader_copter.send_position_to_follower())
+    navigate_thread.daemon = True
+    navigate_thread.start()
 
 
 if __name__ == '__main__':
